882da9a1fe97cad2b7aa6c09ab8c034070a4e921
adelariestra <48964880+adelariestra@users.noreply.github.com>
2022-03-22 09:56:19 -0300

    Started working with arguments

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,26 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 const rootDir = './test/fixture/02_Stdy';
+/*
+Options:
+- MergeType:
+    - f (default): full file
+    - t: todolists only
+    - tp: todolists pending items only
+- ReadType:
+    - i (default): iterate recursivly through subfolders
+    - ni: only root folder.
+
+Parameters:
+- rootDir: origin where to start iterating
+- outputPath: path where to generate the output file.
+*/
+
+// print process.argv
+var args = process.argv.slice(2);
+args.forEach(function (val, index, array) {
+    console.log(index + ': ' + val);
+});
 
 fs.readdir(rootDir, (error,files)=>{
     files.forEach(file=>{
